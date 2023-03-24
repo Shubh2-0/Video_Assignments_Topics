3aee7e5b76ccb30f322f4f0f63b51a565a5f3724
shubham bhati <112773220+Shubh2-0@users.noreply.github.com>
2023-03-25 01:47:26 +0530

    sprint 3 video 1

--- Sprint-3/ByteStream_CharacterStream_1/src/com/masai/Demo.java
+++ Sprint-3/ByteStream_CharacterStream_1/src/com/masai/Demo.java
@@ -1,5 +1,63 @@
 package com.masai;
 
 public class Demo {
+	
+	/*
+	 * BYTE STREAM
+	   |
+	    +-- FilterInputStream
+	    |     |
+	    |     +-- BufferedInputStream
+	    |     +-- DataInputStream
+	    |     +-- PushbackInputStream
+	    |
+	    +-- ByteArrayInputStream
+	    +-- FileInputStream
+	    +-- ObjectInputStream
 
+	OutputStream
+	    |
+	    +-- FilterOutputStream
+	    |     |
+	    |     +-- BufferedOutputStream
+	    |     +-- DataOutputStream
+	    |     +-- PrintStream
+	    |
+	    +-- ByteArrayOutputStream
+	    +-- FileOutputStream
+	    +-- ObjectOutputStream
+	    
+	    
+	    
+	    
+	    
+	Here is a class hierarchy for character stream:
+
+	Reader
+	   |
+	   +-- BufferedReader
+	   |     |
+	   |     +-- LineNumberReader
+	   |
+	   +-- CharArrayReader
+	   +-- InputStreamReader
+	         |
+	         +-- FileReader
+	       
+	 Writer
+   |
+   +-- BufferedWriter
+   |
+   +-- CharArrayWriter
+   |
+   +-- OutputStreamWriter
+         |
+         +-- FileWriter
+        
+	         
+	         
+	         
+
+          
+          */
 }
